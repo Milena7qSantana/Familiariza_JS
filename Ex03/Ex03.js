ea709862bdcddef5e7147300c26a9d61e9ef211b
Dennis Matos <milena.msanta@gmail.com>
2024-09-09 03:01:02 -0300

    Bootstrap Ex03

--- Ex03/Ex03.js
+++ Ex03/Ex03.js
@@ -4,14 +4,18 @@ function enviarIdade() {
   let idade = parseInt(idadeElemento.value);
 
   if (idade < 0 || idade > 130) {
-    document.getElementById("resultado").innerText = "Valor inválido!";
+    document.getElementById("resultado").innerHTML =
+      '<div class="border border-2 border-secondary rounded-pill mt-3 bg-primary w-25"><p class="text-center m-3 fw-bold" style="color: white;">Idade Inválida! <br /> Digite novamente.</p></div>';
   } else {
     if (idade < 16) {
-      document.getElementById("resultado").innerHTML = "Não eleitor!";
+      document.getElementById("resultado").innerHTML =
+        '<div class="border border-2 border-secondary rounded-pill mt-3 bg-primary w-25"><p class="text-center m-3 fw-bold" style="color: white;">Não eleitor!</p></div>';
     } else if (idade > 18 && idade < 65) {
-      document.getElementById("resultado").innerText = "Eleitor obrigatório!";
+      document.getElementById("resultado").innerHTML =
+        '<div class="border border-2 border-secondary rounded-pill mt-3 bg-primary w-25"><p class="text-center m-3 fw-bold" style="color: white;">Eleitor obrigatório!</p></div>';
     } else {
-      document.getElementById("resultado").innerText = "Eleitor facultativo!";
+      document.getElementById("resultado").innerHTML =
+        '<div class="border border-2 border-secondary rounded-pill mt-3 bg-primary w-25"><p class="text-center m-3 fw-bold" style="color: white;">Eleitor facultativo!</p></div>';
     }
   }
 }
